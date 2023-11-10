4cb9603b0a3da8fba143d495f3c035013a985a04
Kostadis999 <72880575+Kostadis999@users.noreply.github.com>
2023-11-10 11:30:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require("path");
-
+app.use(express.static(__dirname + "/css"));
 app.get('/style.css', function(req, res) {
   res.sendFile(__dirname + "/css/" + "styles.css");
 });
